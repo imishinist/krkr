45cb03730e2c819658099a37b2431d297ca43a90
Taisuke Miyazaki <imishinist@gmail.com>
2019-03-30 05:29:42 +0900

    remove unused code

--- krkr.h
+++ krkr.h
@@ -22,20 +22,11 @@ void err_dump(const char *fmt, ...);
 void err_msg(const char *fmt, ...);
 void err_quit(const char *fmt, ...);
 
-int fd_pipe(int fd[2]);
-
-ssize_t readn(int fd, void *buf, size_t nbytes);
-ssize_t writen(int fd, void *buf, size_t nbytes);
-
 int do_driver(int ptym, char *driver);
 
 pid_t
 pty_fork(int *ptrfdm, char *slave_name, size_t slave_namesz,
          const struct termios *slave_termios,
          const struct winsize *slave_winsize);
-
-typedef void Sigfunc(int);
-
-Sigfunc * signal_intr(int signo, Sigfunc *func);
 
 #endif //KRKR_KRKR_H
--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@
 
 #define OPTSTR "+d:p:"
 
-#define BUFSIZE 512
-
-static void sig_term(int);
-static volatile sig_atomic_t sigcaught;
-void loop(int ptym);
 static void set_noecho(int fd);
 
 int main(int argc, char *argv[]) {
@@ -68,66 +63,6 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
-}
-
-
-void
-loop(int ptym)
-{
-    pid_t child;
-    int nread;
-    char buf[BUFSIZE];
-
-    if ((child = fork()) < 0) {
-        err_sys("fork error");
-    } else if (child == 0) {
-        // 標準入力をptymへコピー
-        for (;;) {
-            if ((nread = read(STDIN_FILENO, buf, BUFSIZE)) < 0)
-                err_sys("read error from stdin");
-            else if (nread ==0)
-                break;
-            if (writen(ptym, buf, nread) != nread)
-                err_sys("writen error to master pty");
-        }
-        kill(getppid(), SIGTERM);
-        exit(0);
-    }
-
-    if (signal_intr(SIGTERM, sig_term) == SIG_ERR)
-        err_sys("signal_intr error for SIGTERM");
-
-    for (;;) {
-        if ((nread = read(ptym, buf, BUFSIZE)) <= 0)
-            break;
-        if (writen(STDOUT_FILENO, buf, nread) != nread)
-            err_sys("writen error to stdout");
-    }
-
-    if (sigcaught == 0)
-        kill(child, SIGTERM);
-}
-
-static void
-sig_term(int signo)
-{
-    sigcaught = 1;
-}
-
-Sigfunc *
-signal_intr(int signo, Sigfunc *func)
-{
-    struct sigaction act, oact;
-
-    act.sa_handler = func;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
-#ifdef SA_INTERUPT
-    act.sa_flags |= SA_INTERUPT
-#endif
-    if (sigaction(signo, &act, &oact) < 0)
-        return (SIG_ERR);
-    return oact.sa_handler;
 }
 
 static void
